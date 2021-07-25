8b5814118b13326cdd52f33456194fec02a829c1
John Barker <jdabarker@gmail.com>
2021-07-25 21:34:30 +0800

    added navigation component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Header.css";
 import logo from "../../assets/images/logo.png";
+import Navigation from "../Navigation"
 
 function Header() {
   return (
     <header>
       <img src={logo} alt="logo" />
+      <Navigation></Navigation>
     </header>
   );
 }
