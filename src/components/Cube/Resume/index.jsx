4e3397200c76f1692a4d933ec5d7ec002f905c01
John Barker <jdabarker@gmail.com>
2021-08-21 14:19:46 +0800

    added proficiencies, resume and footer links

--- src/components/Cube/Resume/index.jsx
+++ src/components/Cube/Resume/index.jsx
@@ -4,7 +4,34 @@ function Resume() {
   return (
     <article className="face" id="resume">
       <h1>Resume</h1>
-      <p>resume</p>
+      <p>
+        Download my{" "}
+        <a href="/ResumeJohnBarkerPub.pdf" download>
+          resume
+        </a>
+        .
+      </p>
+      <h3>Proficiencies</h3>
+      <section className="proficiencies">
+        <ul>
+          <li>JavaScript</li>
+          <li>Web Design</li>
+          <li>Node.js</li>
+          <li>C#</li>
+          <li>CSS</li>
+          <li>Git/GitHub</li>
+          <li>Frontent Frameworks - Bootstrap/Bulma</li>
+          <li>Bash</li>
+          <li>ODM/ORM - Mongoose/Sequelize</li>
+          <li>React.js</li>
+          <li>HTML5</li>
+          <li>jQuery</li>
+          <li>Python</li>
+          <li>SQL</li>
+          <li>Express.js</li>
+          <li>Handlebars.js</li>
+        </ul>
+      </section>
     </article>
   );
 }
--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,10 +1,24 @@
 import "./Footer.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Footer() {
   return (
     <footer>
+      <div>© John Barker 2021</div>
       <div>
-        <p>© John Barker 2021</p>
+        <a href="https://github.com/DandyCodes">
+          <FontAwesomeIcon icon={["fab", "github"]}></FontAwesomeIcon>
+        </a>
+      </div>
+      <div>
+        <a href="https://www.linkedin.com/in/johndurackbarker/">
+          <FontAwesomeIcon icon={["fab", "linkedin"]}></FontAwesomeIcon>{" "}
+        </a>
+      </div>
+      <div>
+        <a href="https://stackoverflow.com/users/15135625/dandycodes">
+          <FontAwesomeIcon icon={["fab", "stack-overflow"]}></FontAwesomeIcon>{" "}
+        </a>
       </div>
     </footer>
   );
